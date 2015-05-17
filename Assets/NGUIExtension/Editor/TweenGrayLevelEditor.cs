8b10f675769863b3fe29e7a3a0e255161ced1721
renhao yao <sega752682@gmail.com>
2015-05-18 01:43:51 +0800

    * Update gray level tweener.

---
+++ Assets/NGUIExtension/Editor/TweenGrayLevelEditor.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TweenGrayLevel))]
+public class TweenGrayLevelEditor : UITweenerEditor 
+{
+
+	public override void OnInspectorGUI ()
+	{
+		GUILayout.Space(6f);
+		NGUIEditorTools.SetLabelWidth(120f);
+		
+		TweenGrayLevel tw = target as TweenGrayLevel;
+		GUI.changed = false;
+		
+		float from = EditorGUILayout.FloatField("From", tw.from);
+		float to = EditorGUILayout.FloatField("To", tw.to);
+		//bool table = EditorGUILayout.Toggle("Update Table", tw.updateTable);
+		
+		if (GUI.changed)
+		{
+			NGUIEditorTools.RegisterUndo("Tween Change", tw);
+			tw.from = from;
+			tw.to = to;
+			//tw.updateTable = table;
+			UnityEditor.EditorUtility.SetDirty(tw);
+		}
+		
+		DrawCommonProperties();
+	}
+}
---
+++ Assets/NGUIExtension/TweenGrayLevel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("NGUI/Tween/Tween Graylevel")]
+public class TweenGrayLevel : UITweener {
+	public float from;
+	public float to;
+	
+	UITexture mUITexture;
+	public UITexture CachedUITexture { get { if (mUITexture == null) mUITexture = GetComponent<UITexture>(); return mUITexture; } }
+	public Material material { get { return CachedUITexture.drawCall.dynamicMaterial; } }
+	override protected void OnUpdate (float factor, bool isFinished) {		
+		float newPoint = from * (1f - factor) + to * factor;
+
+		if(CachedUITexture == null) return;
+		if(CachedUITexture.drawCall == null) return;
+		if(material == null) return;
+
+		material.SetFloat("_GrayLevelScale", newPoint);
+	}
+	
+	/// <summary>
+	/// Start the tweening operation.
+	/// </summary>
+	
+	static public TweenGrayLevel Begin (GameObject go, float duration, float newPoint)
+	{
+		TweenGrayLevel comp = UITweener.Begin<TweenGrayLevel>(go, duration);
+		
+		comp.from = comp.material.GetFloat("_GrayLevelScale");
+		comp.to = newPoint;
+		
+		if (duration <= 0f)
+		{
+			comp.Sample(1f, true);
+			comp.enabled = false;
+		}
+		return comp;
+	}
+}
